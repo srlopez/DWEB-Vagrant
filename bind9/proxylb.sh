28c929bf457c40a45b132597e2e9d2b1c28d9516
srlopez <srlopezh@gmail.com>
2022-11-19 13:05:06 +0100

    virtuales, balanceador y proxy inverso

---
+++ bind9/proxylb.sh
@@ -0,0 +1,36 @@
+cat <<EOF >/etc/nginx/sites-available/000-default
+# Pasamos la dirección del cliente
+proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+upstream MisBALANCEADOS {
+    server apache1.aula104.local;
+    server apache2.aula104.local;
+    server sv1;
+    server sv2;
+    server 192.168.1.11:8080;
+    server 10.0.0.12:8080;
+}
+
+server {
+    listen 80;
+
+    # En / redirijo a los servidores de carga
+    location / {
+        proxy_pass http://MisBALANCEADOS;
+    }
+
+    # Proxy inverso a apache1 en /uno
+    location /uno {
+        proxy_pass http://apache1/;
+    }
+
+    # Proxy inverso a apache2 en /dos
+    location /dos {
+        proxy_pass http://apache2/;
+    }
+}
+EOF
+
+cd /etc/nginx/sites-enabled
+rm default
+ln -s ../sites-available/000-default 
+systemctl restart nginx
---
+++ bind9/virtualhosts.sh
@@ -0,0 +1,69 @@
+ID=$1
+IP1=$2
+IP2=$3
+DOM=$4
+
+# Instala el módulo php para apache y el php correspondiente
+apt install -y libapache2-mod-php 
+# Preparamos index.php
+mv /var/www/html/index.html /var/www/html/index.html.org
+cat <<EOF >/var/www/html/index.php
+<style>*{font-family:Consolas,monaco,monospace; font-size: 20px;}</style><pre>
+<?php print
+"SERVER_NAME     ".\$_SERVER['SERVER_NAME']."\n".
+"SERVER_ADDR     ".\$_SERVER['SERVER_ADDR']."\n".
+"SERVER_PORT     ".\$_SERVER['SERVER_PORT']."\n".
+"HTTP_HOST       ".\$_SERVER['HTTP_HOST']."\n".
+"REQUEST_URI     ".\$_SERVER['REQUEST_URI']."\n".
+"SCRIPT_FILENAME ".\$_SERVER['SCRIPT_FILENAME']."\n".
+"REMOTE_ADDR     ".\$_SERVER['REMOTE_ADDR']."\n".
+"X_FORWARDED_FOR ".\$_SERVER['HTTP_X_FORWARDED_FOR']."\n".
+"The time is     " . date("h:i:sa")."\n"
+?>
+</pre>
+EOF
+
+# Preparamos la configuración de hosts
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.org
+cat <<EOF >/etc/apache2/sites-available/000-default.conf
+# Discriminamos por ServerName
+<VirtualHost *:80>
+  DocumentRoot "/var/www/80"
+</VirtualHost>
+<VirtualHost *:80>
+  DocumentRoot "/var/www/apache$ID"
+  ServerName apache$ID.$DOM
+  ServerAlias apache$ID
+</VirtualHost>
+<VirtualHost *:80>
+  DocumentRoot "/var/www/sv$ID"
+  ServerName sv$ID.$DOM
+  ServerAlias sv$ID
+</VirtualHost>
+
+# Lo que venga por IP:8080
+Listen 8080
+<VirtualHost *:8080>
+  DocumentRoot "/var/www/8080"
+</VirtualHost>
+# Discriminamos por IP
+<VirtualHost $IP1:8080>
+  DocumentRoot "/var/www/192"
+</VirtualHost>
+<VirtualHost $IP2:8080>
+  DocumentRoot "/var/www/10"
+</VirtualHost>
+EOF
+
+# Creamos los directorios de las aplicaciones
+# todos igual ya que variaran a la hora de mostrar la información
+cd /var/www/
+ln -s html 80
+ln -s html apache$ID
+ln -s html sv$ID
+ln -s html 8080
+ln -s html 10
+ln -s html 192
+systemctl restart apache2
+curl -s apache$ID
+curl -s $IP2:8080
